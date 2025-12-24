38c6688141c3a2a101a4ce9a5b3fb710056ec349
Steven Clontz <steven.clontz@gmail.com>
2025-12-24 17:20:57 +0000

    add placeholder subscribe button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,21 @@ class PagesController < ApplicationController
   def home
     render layout: false
   end
+
+  def subscribe
+    require "stripe"
+    Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+
+    session = Stripe::Checkout::Session.create({
+      line_items: [ {
+        # Provide the exact Price ID (for example, price_1234) of the product you want to sell
+        price: "pretextplus_sustaining",
+        quantity: 1
+      } ],
+      mode: "subscription",
+      success_url: "https://pretext.plus/session"
+    })
+
+    redirect_to session.url, allow_other_host: true
+  end
 end

3e63f0db308e1f4e9337216f4c5e45d78bf35327
Steven Clontz <steven.clontz@gmail.com>
2026-01-07 16:21:16 +0000

    clean up password reset flow

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -20,7 +20,8 @@ class PasswordsController < ApplicationController
   def update
     if @user.update(params.permit(:password, :password_confirmation))
       @user.sessions.destroy_all
-      redirect_to new_session_path, notice: "Password has been reset."
+      start_new_session_for @user
+      redirect_to projects_path, notice: "Password has been reset. You are now logged in."
     else
       redirect_to edit_password_path(params[:token]), alert: "Passwords did not match."
     end
